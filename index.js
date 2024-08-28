c28121382c37fa9738b3c7d85487258e948b099d
marco-soria <marco.soriaa7@gmail.com>
2024-08-28 17:51:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log('Server is running on port 3001');
+    });
